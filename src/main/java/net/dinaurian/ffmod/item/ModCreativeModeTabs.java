908d9320ade89c5a0bf76c30d6d760e8c1fe0c86
Pokeexplorer123 <vihanshah07@gmail.com>
2025-03-25 13:29:56 +0530

    added all vivos

--- src/main/java/net/dinaurian/ffmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/dinaurian/ffmod/item/ModCreativeModeTabs.java
@@ -14,10 +14,128 @@ public class ModCreativeModeTabs {
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FFMod.MOD_ID);
 
     public static final RegistryObject<CreativeModeTab> FF_ITEMS_TAB = CREATIVE_MODE_TABS.register("ff_items_tab",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.ALLOSPRITE.get()))
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.TREXSPRITE.get()))
                     .title(Component.translatable("creativetab.ffmod.ff_items"))
                     .displayItems((itemDisplayParameters, output) -> {
+                        output.accept(ModItems.TREXSPRITE.get());
+                        output.accept(ModItems.DASPLETOSPRITE.get());
+                        output.accept(ModItems.GORGOSPRITE.get());
+                        output.accept(ModItems.TARBOSPRITE.get());
+                        output.accept(ModItems.ALIOSPRITE.get());
+                        output.accept(ModItems.SIAMOSPRITE.get());
+                        output.accept(ModItems.ALECTROSPRITE.get());
+                        output.accept(ModItems.GUANSPRITE.get());
+                        output.accept(ModItems.SHANSHANSPRITE.get());
                         output.accept(ModItems.ALLOSPRITE.get());
+                        output.accept(ModItems.METRIASPRITE.get());
+                        output.accept(ModItems.MEGALOSPRITE.get());
+                        output.accept(ModItems.VENATORSPRITE.get());
+                        output.accept(ModItems.SRAPTORSPRITE.get());
+                        output.accept(ModItems.GIGANTOSPRITE.get());
+                        output.accept(ModItems.CRYOSPRITE.get());
+                        output.accept(ModItems.CARCHARSPRITE.get());
+                        output.accept(ModItems.ACROSPRITE.get());
+                        output.accept(ModItems.FRAPTORSPRITE.get());
+                        output.accept(ModItems.SPINAXSPRITE.get());
+                        output.accept(ModItems.NEOSPRITE.get());
+                        output.accept(ModItems.COMPSOSPRITE.get());
+                        output.accept(ModItems.SOPTERYXSPRITE.get());
+                        output.accept(ModItems.DELTASPRITE.get());
+                        output.accept(ModItems.TROSPRITE.get());
+                        output.accept(ModItems.NYCHUSSPRITE.get());
+                        output.accept(ModItems.MRAPTORSPRITE.get());
+                        output.accept(ModItems.URAPTORSPRITE.get());
+                        output.accept(ModItems.VRAPTORSPRITE.get());
+                        output.accept(ModItems.BREMESPRITE.get());
+                        output.accept(ModItems.AOPTERYXSPRITE.get());
+                        output.accept(ModItems.COELOSPRITE.get());
+                        output.accept(ModItems.DILOPHOSPRITE.get());
+                        output.accept(ModItems.SPINOSPRITE.get());
+                        output.accept(ModItems.ANGATOSPRITE.get());
+                        output.accept(ModItems.SUCHOSPRITE.get());
+                        output.accept(ModItems.ONYXSPRITE.get());
+                        output.accept(ModItems.CERATOSPRITE.get());
+                        output.accept(ModItems.CARNOSPRITE.get());
+                        output.accept(ModItems.ORNOSPRITE.get());
+                        output.accept(ModItems.CHEIRUSSPRITE.get());
+                        output.accept(ModItems.ORAPTORSPRITE.get());
+                        output.accept(ModItems.ZINOSPRITE.get());
+                        output.accept(ModItems.BRACHIOSPRITE.get());
+                        output.accept(ModItems.SALTOSPRITE.get());
+                        output.accept(ModItems.SHUNOSPRITE.get());
+                        output.accept(ModItems.PERSOSPRITE.get());
+                        output.accept(ModItems.SEISMOSPRITE.get());
+                        output.accept(ModItems.APATOSPRITE.get());
+                        output.accept(ModItems.AMARGOSPRITE.get());
+                        output.accept(ModItems.STEGOSPRITE.get());
+                        output.accept(ModItems.YANGOSPRITE.get());
+                        output.accept(ModItems.JIANGOSPRITE.get());
+                        output.accept(ModItems.KENTROSPRITE.get());
+                        output.accept(ModItems.LEXOSPRITE.get());
+                        output.accept(ModItems.NODOSPRITE.get());
+                        output.accept(ModItems.ANKYLOSPRITE.get());
+                        output.accept(ModItems.SAICHANSPRITE.get());
+                        output.accept(ModItems.GOYLESPRITE.get());
+                        output.accept(ModItems.PELTOSPRITE.get());
+                        output.accept(ModItems.HYPSISPRITE.get());
+                        output.accept(ModItems.NASAURSPRITE.get());
+                        output.accept(ModItems.IGUASPRITE.get());
+                        output.accept(ModItems.OURANOSPRITE.get());
+                        output.accept(ModItems.LAMBEOSPRITE.get());
+                        output.accept(ModItems.MAIASPRITE.get());
+                        output.accept(ModItems.ANATOSPRITE.get());
+                        output.accept(ModItems.PARALOPHSPRITE.get());
+                        output.accept(ModItems.PACHYSPRITE.get());
+                        output.accept(ModItems.STYGISPRITE.get());
+                        output.accept(ModItems.GOYOSPRITE.get());
+                        output.accept(ModItems.PROTOSPRITE.get());
+                        output.accept(ModItems.TRICERASPRITE.get());
+                        output.accept(ModItems.STYRACOSPRITE.get());
+                        output.accept(ModItems.EINIOSPRITE.get());
+                        output.accept(ModItems.CENTROSPRITE.get());
+                        output.accept(ModItems.PENTASPRITE.get());
+                        output.accept(ModItems.PACHRINOSPRITE.get());
+                        output.accept(ModItems.MIHUSPRITE.get());
+                        output.accept(ModItems.PTERASPRITE.get());
+                        output.accept(ModItems.COATLUSSPRITE.get());
+                        output.accept(ModItems.JARASPRITE.get());
+                        output.accept(ModItems.DIMORPHSPRITE.get());
+                        output.accept(ModItems.GUERASPRITE.get());
+                        output.accept(ModItems.CRONASPRITE.get());
+                        output.accept(ModItems.FUTABISPRITE.get());
+                        output.accept(ModItems.ELASMOSPRITE.get());
+                        output.accept(ModItems.PLESIOSPRITE.get());
+                        output.accept(ModItems.SHONISPRITE.get());
+                        output.accept(ModItems.ARSITHSPRITE.get());
+                        output.accept(ModItems.BRONTOTHSPRITE.get());
+                        output.accept(ModItems.ELASMOTHSPRITE.get());
+                        output.accept(ModItems.HOPLOSPRITE.get());
+                        output.accept(ModItems.ANDRARCHSPRITE.get());
+                        output.accept(ModItems.PAKISPRITE.get());
+                        output.accept(ModItems.SMILOSPRITE.get());
+                        output.accept(ModItems.MAMMOTHSPRITE.get());
+                        output.accept(ModItems.TRYMASPRITE.get());
+                        output.accept(ModItems.MEGATHSPRITE.get());
+                        output.accept(ModItems.CHELONSPRITE.get());
+                        output.accept(ModItems.DINOMATONSPRITE.get());
+                        output.accept(ModItems.DUNASPRITE.get());
+                        output.accept(ModItems.RAPTINSPRITE.get());
+                        output.accept(ModItems.DYNALSPRITE.get());
+                        output.accept(ModItems.FRIGISPRITE.get());
+                        output.accept(ModItems.IGNOSPRITE.get());
+                        output.accept(ModItems.SQUIKSPRITE.get());
+                        output.accept(ModItems.SQUIRKSPRITE.get());
+                        output.accept(ModItems.SQUIRTHSPRITE.get());
+                        output.accept(ModItems.SQUILKSPRITE.get());
+                        output.accept(ModItems.SQUIROSPRITE.get());
+                        output.accept(ModItems.GUHWEEPSPRITE.get());
+                        output.accept(ModItems.GUHVORNSPRITE.get());
+                        output.accept(ModItems.GUHLITHSPRITE.get());
+
+
+
+
+
 
                     }).build());
 
--- src/main/java/net/dinaurian/ffmod/item/ModItems.java
+++ src/main/java/net/dinaurian/ffmod/item/ModItems.java
@@ -11,7 +11,346 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, FFMod.MOD_ID);
 
+    public static final RegistryObject<Item> TREXSPRITE = ITEMS.register("trexsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DASPLETOSPRITE = ITEMS.register("daspletosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GORGOSPRITE = ITEMS.register("gorgosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TARBOSPRITE = ITEMS.register("tarbosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ALIOSPRITE = ITEMS.register("aliosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SIAMOSPRITE = ITEMS.register("siamosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ALECTROSPRITE = ITEMS.register("alectrosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GUANSPRITE = ITEMS.register("guansprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SHANSHANSPRITE = ITEMS.register("shanshansprite",
+            () -> new Item(new Item.Properties()));
+
     public static final RegistryObject<Item> ALLOSPRITE = ITEMS.register("allosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> METRIASPRITE = ITEMS.register("metriasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> MEGALOSPRITE = ITEMS.register("megalosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> VENATORSPRITE = ITEMS.register("venatorsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SRAPTORSPRITE = ITEMS.register("sraptorsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GIGANTOSPRITE = ITEMS.register("gigantosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CRYOSPRITE = ITEMS.register("cryosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CARCHARSPRITE = ITEMS.register("carcharsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ACROSPRITE = ITEMS.register("acrosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> FRAPTORSPRITE = ITEMS.register("fraptorsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SPINAXSPRITE = ITEMS.register("spinaxsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> NEOSPRITE = ITEMS.register("neosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> COMPSOSPRITE = ITEMS.register("compsosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SOPTERYXSPRITE = ITEMS.register("sopteryxsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DELTASPRITE = ITEMS.register("deltasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TROSPRITE = ITEMS.register("trosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> NYCHUSSPRITE = ITEMS.register("nychussprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> MRAPTORSPRITE = ITEMS.register("mraptorsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> URAPTORSPRITE = ITEMS.register("uraptorsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> VRAPTORSPRITE = ITEMS.register("vraptorsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BREMESPRITE = ITEMS.register("bremesprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> AOPTERYXSPRITE = ITEMS.register("aopteryxsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> COELOSPRITE = ITEMS.register("coelosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DILOPHOSPRITE = ITEMS.register("dilophosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SPINOSPRITE = ITEMS.register("spinosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ANGATOSPRITE = ITEMS.register("angatosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SUCHOSPRITE = ITEMS.register("suchosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ONYXSPRITE = ITEMS.register("onyxsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CERATOSPRITE = ITEMS.register("ceratosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CARNOSPRITE = ITEMS.register("carnosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ORNOSPRITE = ITEMS.register("ornosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CHEIRUSSPRITE = ITEMS.register("cheirussprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ORAPTORSPRITE = ITEMS.register("oraptorsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ZINOSPRITE = ITEMS.register("zinosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BRACHIOSPRITE = ITEMS.register("brachiosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SALTOSPRITE = ITEMS.register("saltosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SHUNOSPRITE = ITEMS.register("shunosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PERSOSPRITE = ITEMS.register("persosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SEISMOSPRITE = ITEMS.register("seismosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> APATOSPRITE = ITEMS.register("apatosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> AMARGOSPRITE = ITEMS.register("amargosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> STEGOSPRITE = ITEMS.register("stegosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> YANGOSPRITE = ITEMS.register("yangosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> JIANGOSPRITE = ITEMS.register("jiangosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> KENTROSPRITE = ITEMS.register("kentrosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> LEXOSPRITE = ITEMS.register("lexosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> NODOSPRITE = ITEMS.register("nodosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ANKYLOSPRITE = ITEMS.register("ankylosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SAICHANSPRITE = ITEMS.register("saichansprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GOYLESPRITE = ITEMS.register("goylesprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PELTOSPRITE = ITEMS.register("peltosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> HYPSISPRITE = ITEMS.register("hypsisprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> NASAURSPRITE = ITEMS.register("nasaursprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> IGUASPRITE = ITEMS.register("iguasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> OURANOSPRITE = ITEMS.register("ouranosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> LAMBEOSPRITE = ITEMS.register("lambeosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> MAIASPRITE = ITEMS.register("maiasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ANATOSPRITE = ITEMS.register("anatosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PARALOPHSPRITE = ITEMS.register("paralophsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PACHYSPRITE = ITEMS.register("pachysprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> STYGISPRITE = ITEMS.register("stygisprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GOYOSPRITE = ITEMS.register("goyosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PROTOSPRITE = ITEMS.register("protosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TRICERASPRITE = ITEMS.register("tricerasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> STYRACOSPRITE = ITEMS.register("styracosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> EINIOSPRITE = ITEMS.register("einiosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CENTROSPRITE = ITEMS.register("centrosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PENTASPRITE = ITEMS.register("pentasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PACHRINOSPRITE = ITEMS.register("pachrinosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> MIHUSPRITE = ITEMS.register("mihusprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PTERASPRITE = ITEMS.register("pterasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> COATLUSSPRITE = ITEMS.register("coatlussprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> JARASPRITE = ITEMS.register("jarasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DIMORPHSPRITE = ITEMS.register("dimorphsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GUERASPRITE = ITEMS.register("guerasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CRONASPRITE = ITEMS.register("cronasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> FUTABISPRITE = ITEMS.register("futabisprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ELASMOSPRITE = ITEMS.register("elasmosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PLESIOSPRITE = ITEMS.register("plesiosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SHONISPRITE = ITEMS.register("shonisprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ARSITHSPRITE = ITEMS.register("arsithsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> BRONTOTHSPRITE = ITEMS.register("brontothsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ELASMOTHSPRITE = ITEMS.register("elasmothsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> HOPLOSPRITE = ITEMS.register("hoplosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> ANDRARCHSPRITE = ITEMS.register("andrarchsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PAKISPRITE = ITEMS.register("pakisprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SMILOSPRITE = ITEMS.register("smilosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> MAMMOTHSPRITE = ITEMS.register("mammothsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> TRYMASPRITE = ITEMS.register("trymasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> MEGATHSPRITE = ITEMS.register("megathsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> CHELONSPRITE = ITEMS.register("chelonsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DINOMATONSPRITE = ITEMS.register("dinomatonsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DUNASPRITE = ITEMS.register("dunasprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAPTINSPRITE = ITEMS.register("raptinsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> DYNALSPRITE = ITEMS.register("dynalsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> FRIGISPRITE = ITEMS.register("frigisprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> IGNOSPRITE = ITEMS.register("ignosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SQUIKSPRITE = ITEMS.register("squiksprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SQUIRKSPRITE = ITEMS.register("squirksprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SQUIRTHSPRITE = ITEMS.register("squirthsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SQUILKSPRITE = ITEMS.register("squilksprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SQUIROSPRITE = ITEMS.register("squirosprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GUHWEEPSPRITE = ITEMS.register("guhweepsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GUHVORNSPRITE = ITEMS.register("guhvornsprite",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> GUHLITHSPRITE = ITEMS.register("guhlithsprite",
             () -> new Item(new Item.Properties()));
 
 
